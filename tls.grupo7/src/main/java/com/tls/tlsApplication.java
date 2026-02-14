ee549ab0aaece697874e71826b6a0e2b29101768
aleperez5061 <145790180+aleperez5061@users.noreply.github.com>
2026-02-13 21:38:49 -0700

    Logo colores y letra con la marca

---
+++ tls.grupo7/src/main/java/com/tls/tlsApplication.java
@@ -0,0 +1,13 @@
+package com.tls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tlsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(tlsApplication.class, args);
+    }
+
+}
